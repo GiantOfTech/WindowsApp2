b6796fa81315f6139c09be66c6ecbadcc2b8f2c6
mahfuz <mahfuz@LAPTOP-LMPEH6IU>
2021-02-10 17:31:59 +0200

    1

--- WindowsApp2/Form1.vb
+++ WindowsApp2/Form1.vb
@@ -2,4 +2,9 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label1.Text = "okkkkk"
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("123")
+
+    End Sub
 End Class
